d6aaee2ed5ee821b550b18b2b879bd5c26c459c8
Iqramulya <iqragading.ig@gmail.com>
2024-06-20 11:02:36 +0800

    inventory 59%

--- app/src/main/java/com/dicoding/InsightCart/ui/Inventory/Records/RecordsFragment.kt
+++ app/src/main/java/com/dicoding/InsightCart/ui/Inventory/Records/RecordsFragment.kt
@@ -1,7 +1,35 @@
 package com.dicoding.InsightCart.ui.Inventory.Records
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.dicoding.InsightCart.databinding.FragmentRecordsInventoryBinding
 
 class RecordsFragment : Fragment() {
 
+    private var _binding: FragmentRecordsInventoryBinding? = null
+    private val binding get() = _binding!!
+
+    private val recordList = ArrayList<Record>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentRecordsInventoryBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        binding.recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        binding.recyclerView.adapter = RecordAdapter(recordList)
+
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
